c495e88ec88a5611755b63ecd954acffafaaf5e4
Matthew Li <matthewli2491@gmail.com>
2020-02-09 16:31:39 -0500

    added grid prop to struct

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -18,12 +18,18 @@ defmodule Identicon do
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3) # turning a list into a list of list
+    grid = 
+      hex
+      |> Enum.chunk(3) # turning a list into a list of list
+      |> Enum.map(&mirror_row/1) # calling a reference to the function mirror_row with 1 argument
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do 
     [first, second | _tail] = row
-    row ++ [second, first]
+    row ++ [second, first]  
   end
 end
